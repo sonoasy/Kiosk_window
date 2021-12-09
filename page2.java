75460004fdaf5a379f8c0b10a394b6ec1d450b5a
seoyeon ahn <61787171+sonoasy@users.noreply.github.com>
2021-12-09 13:00:16 +0900

    메뉴판 화면

--- page2.java
+++ page2.java
@@ -8,15 +8,22 @@ import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.GridLayout;
 import java.awt.Image;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
+import java.io.File;
 
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JSplitPane;
@@ -28,21 +35,6 @@ import javax.swing.table.DefaultTableModel;
 
 import kiosk_window.page2.MymouseListener;
 
-//공유되는 장바구니, hashmap
-class sharedbox{
-	
-	//public static JTable buylist;	
-	//static 장바구니 만들기
-	public sharedbox() {
-	
-
-	}
-	
-	
-	
-}
-
-
 class page2_two extends JPanel{
 	//세트 메뉴판
 	
@@ -50,12 +42,12 @@ class page2_two extends JPanel{
      public page2_two(){
 
          setLayout(null);
-	    
+         
 	    //loop문으로 바꾸기
 	    String setmenu[]= {"1955 버거 세트","더블 불고기 버거 세트","더블 치즈버거 세트","더블 쿼터파운더 치즈 세트","더블 필레오피쉬 세트","맥시파이시 상하이 버거 세트","맥치킨 모짜렐라 세트","맥치킨 세트","베이컨 토마토 디럭스 세트","불고기 버거 세트","빅맥 세트","슈비 버거 세트","슈슈 버거 세트","에그 불고기 버거 세트","창녕 갈릭 버거 세트","치즈버거 세트"};
 	    for(int i=0;i<setmenu.length;i++) {
 		//버튼 당 쓸 이미지 
-		  ImageIcon drink1=new ImageIcon("C:\\Users\\AhnSeoYeon\\Desktop\\workspace\\ch09\\src\\ch09\\"+setmenu[i]+".PNG");
+		  ImageIcon drink1=new ImageIcon("C:\\Users\\AhnSeoYeon\\Desktop\\workspace\\ch09\\src\\ch09\\"+setmenu[i]+".PNG"); //깃허브 menu 폴더의 경로를 설정해주어야 합니다.
           Image set1img =drink1.getImage();
           Image newset1=set1img.getScaledInstance(180, 100, java.awt.Image.SCALE_SMOOTH);
           ImageIcon newnewset1=new ImageIcon(newset1);
@@ -64,7 +56,7 @@ class page2_two extends JPanel{
 		  //이미지 버튼에 추가하기
           setno1[i]=new JButton(setmenu[i]);
           MymouseListener listener = new MymouseListener();
-          setno1[i].addMouseListener(listener);
+          
  
           setno1[i].setLayout(new BorderLayout());
           setno1[i].add(set_1,BorderLayout.CENTER);
@@ -85,8 +77,9 @@ class page2_two extends JPanel{
           setno1[i].setBackground(Color.WHITE);
           setno1[i].setFont(new Font("고딕체", Font.BOLD, 15));
           setno1[i].setBorderPainted(false);
-		
+        
 		add(setno1[i]);
+		add(page2.kiosk);
 		setVisible(true);
 		
 	}
@@ -101,6 +94,10 @@ class page2_two extends JPanel{
  		record[1]="1";
  		record[2]=setprice[tmpt];//burgerprice[tmpt];
  		page2.prices+=Integer.parseInt(record[2]);
+ 		
+ 		page2.list+=record[0]+"   "+record[1]+"   "+record[2]+"원\n";
+ 		
+ 		page2.kiosk.setText(record[0]+" "+record[1]+"개 주문하셨습니다.");
  		model.addRow(record);
  		model2.addRow(record);
  	}
@@ -152,7 +149,7 @@ class page2_three extends JPanel{
 	    String drinkmenu[]= {"드립 커피","디카페인 아메리카노","디카페인 아이스 아메리카노","디카페인 아이스 카페라떼","디카페인 에스프레소","디카페인 카페라떼","디카페인 카푸치노","딸기 쉐이크 Medium","배 칠러","바닐라 쉐이크 Medium","배 칠러","생수","스프라이트","아메리카노","아이스 드립 커피","아이스 아메리카노"};
 	    for(int i=0;i<drinkmenu.length;i++) {
 		//버튼 당 쓸 이미지 
-		  ImageIcon drink1=new ImageIcon("C:\\Users\\AhnSeoYeon\\Desktop\\workspace\\ch09\\src\\ch09\\"+drinkmenu[i]+".PNG");
+		  ImageIcon drink1=new ImageIcon("C:\\Users\\AhnSeoYeon\\Desktop\\workspace\\ch09\\src\\ch09\\"+drinkmenu[i]+".PNG"); //깃허브 menu폴더를 다운받아 경로를 설정해주어야 합니다
           Image drink1img =drink1.getImage();
           Image newdrink1=drink1img.getScaledInstance(180, 100, java.awt.Image.SCALE_SMOOTH);
           ImageIcon newnewdrink1=new ImageIcon(newdrink1);
@@ -182,11 +179,13 @@ class page2_three extends JPanel{
           drinkno1[i].setBackground(Color.WHITE);
           drinkno1[i].setFont(new Font("고딕체", Font.BOLD, 15));
           drinkno1[i].setBorderPainted(false);
-		
+        
 		add(drinkno1[i]);
-		setVisible(true);
+		
 		
 	}
+	    add(page2.kiosk);
+        setVisible(true);
     }
     public void addRecord(int tmpt) {
 		DefaultTableModel model=(DefaultTableModel)page2.table.getModel();
@@ -197,9 +196,42 @@ class page2_three extends JPanel{
 		record[0]=drinkno1[tmpt].getText();
 		record[1]="1";
 		record[2]=drinkprice[tmpt];//burgerprice[tmpt];
+		
+		page2.kiosk.setText("네, "+record[0]+" "+record[1]+"잔 드리겠습니다.");
 		page2.prices+=Integer.parseInt(record[2]);
+		page2.list+=record[0]+"   "+record[1]+"   "+record[2]+"원\n";
 		model.addRow(record);
 		model2.addRow(record);
+		
+		ImageIcon kimg= new ImageIcon("C:\\Users\\AhnSeoYeon\\Desktop\\kioya.png"); //가상 ai점원 이미지로 깃허브에서 kioya.png를 다운받아 경로를 설정해 주어야 합니다. 
+		Image kkimg =kimg.getImage();
+        Image kkkimg=kkimg.getScaledInstance(200, 200, java.awt.Image.SCALE_SMOOTH);
+        ImageIcon kkkkimg=new ImageIcon(kkkimg);
+        JLabel img_1=new JLabel(kkkkimg,JLabel.CENTER);
+		JFrame f = new JFrame("키오야");        
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        f.setLocationRelativeTo(null);
+        f.setSize(300,300);
+        f.add(img_1);
+        f.setVisible(true);            
+          
+        File file = new File("C:\\Users\\AhnSeoYeon\\Desktop\\sample_audio.wav");
+        System.out.println(file.exists()); //true
+        
+        try {
+            
+            AudioInputStream stream = AudioSystem.getAudioInputStream(file);
+            Clip clip = AudioSystem.getClip();
+            clip.open(stream);
+            clip.start();
+            
+        } catch(Exception e) {
+            
+            e.printStackTrace();
+        }
+
+		
+		
 		
 	}
     class MymouseListener implements MouseListener,KeyListener{	
@@ -246,16 +278,13 @@ class buylist extends JPanel{
 		//장바구니 칸 , 총합계는 마지막에
 		String rows[][] = { {} };
 		    // 항목 값
-		String headers[] = { "상품명","수량","단가","합계"};
+		String headers[] = { "상품명","수량","단가"};
 		JTable table = new JTable(new DefaultTableModel(rows, headers));
 		JScrollPane scrollPane = new JScrollPane(table);
 		add(scrollPane);
 		//pack();
 		
-		JButton additem=new JButton("추가");
-		JButton deleteitem=new JButton("삭제");
-		add(additem);
-		add(deleteitem);
+	
 		setVisible(true);
 		
 		
@@ -270,18 +299,19 @@ class buylist extends JPanel{
 public class page2 extends JFrame {
 	public static JTable table;	
 	public static JTable buylist;
+	public static String list="";
 	public static int prices=0;
+	public static JLabel kiosk=new JLabel();
 	public JButton [] burgerno1=new JButton[16];
 	public String burgerprice[]= {"5700","4400","4400","7000","5000","4600","4800","3300","5500","2200","4600","5500","4500","3200","6100","2000"};
     
-	
+	public static String text="";
 	class page2_one extends JPanel{
 		//단품 메뉴판
 		
 		
 		public page2_one(){
 			
-			//setPreferredSize(new Dimension(500, 300));
 		    setLayout(null);
 		    
 		    
@@ -290,7 +320,7 @@ public class page2 extends JFrame {
 		    String burgermenu[]= {"1955 버거","더블 불고기 버거","더블 치즈버거","더블 쿼터파운더 치즈버거","더블 필레오피쉬버거","맥스파이시 상하이 버거","맥치킨 모짜렐라 버거","맥치킨 버거","베이컨 토마토 디럭스 버거","불고기 버거","빅맥 버거","슈비 버거","슈슈 버거","에그 불고기 버거","창녕 갈릭 버거","치즈버거"};
 		    for(int i=0;i<burgermenu.length;i++) {
 			//버튼 당 쓸 이미지 
-			  ImageIcon burger1=new ImageIcon("C:\\Users\\AhnSeoYeon\\Desktop\\workspace\\ch09\\src\\ch09\\"+burgermenu[i]+".PNG");
+			  ImageIcon burger1=new ImageIcon("C:\\Users\\AhnSeoYeon\\Desktop\\workspace\\ch09\\src\\ch09\\"+burgermenu[i]+".PNG"); //깃허브 menu에서 다운받은 이미지들의 경로를 지정해주어야 합니다
 	          Image burger1img =burger1.getImage();
 	          Image newburger1=burger1img.getScaledInstance(180, 100, java.awt.Image.SCALE_SMOOTH);
 	          ImageIcon newnewburger1=new ImageIcon(newburger1);
@@ -346,9 +376,7 @@ public class page2 extends JFrame {
 		page2_one one=new page2_one();
 
 		page2_two two=new page2_two();
-		//two.setPreferredSize(new Dimension(500, 300));
 		page2_three three=new page2_three();
-		//three.setPreferredSize(new Dimension(500, 300));
 		JTabbedPane menutab=new JTabbedPane(JTabbedPane.TOP);
 	    //메뉴탭으로 하지말고 버튼으로 바꾸자
 		menutab.addTab("단품",one);
@@ -379,8 +407,6 @@ public class page2 extends JFrame {
 		
 		JPanel btns=new JPanel();
 		btns.setLayout(new BorderLayout());
-		//btns.add("East",addBtn);
-		//btns.add("West",delBtn);
 		right.add("South",btns);
 		split.setRightComponent(right);
 		
@@ -395,13 +421,12 @@ public class page2 extends JFrame {
 		price_total.setText("총 주문 금액:  "+prices);
 		
 		buy_list.add("South",price_total);
+		
+		
+		
 		//주문내역창 팝업으로 설정하기 
 		
-		
-		
-		//split.setRightComponent(addBtn);
-		
-		//split.setRightComponent(cart);
+	
 		Dimension d= split.getSize();
 	    split.setDividerLocation(830);
 		
@@ -416,6 +441,16 @@ public class page2 extends JFrame {
 		before.setSize(150,50);
 		
 		JButton end=new JButton("주문 완료");
+		end.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				
+				
+				JOptionPane.showMessageDialog(null,"************주문내역************\n"+list+"총 주문 금액:  "+prices+"원");
+				
+			}
+		});
 	
 		end.setSize(150,50);
 		
@@ -423,15 +458,15 @@ public class page2 extends JFrame {
 	    r.add(end,BorderLayout.EAST);
 		split2.setBottomComponent(r);
 		split2.setDividerLocation(900);
-		JLabel kiosk=new JLabel();
 		
-		kiosk.setBounds(294,850,700,50);   //x,y, 가로,세로
+		
+		kiosk.setBounds(120,830,700,50);   //x,y, 가로,세로
 		kiosk.setOpaque(true);
 		kiosk.setHorizontalAlignment(JLabel.CENTER);
-        kiosk.setFont(new Font("고딕체", Font.BOLD, 40));
+        kiosk.setFont(new Font("고딕체", Font.BOLD, 30));
         kiosk.setForeground(Color.WHITE);
         kiosk.setBackground(Color.BLACK);
-        add(kiosk);
+        
 		add(split2);
 		
 		
@@ -444,9 +479,13 @@ public class page2 extends JFrame {
 		String []record=new String[4];
 		
 		record[0]=burgerno1[tmpt].getText();
+		
 		record[1]="1";
+		text=record[0]+" "+record[1]+"개 주문하셨습니다.";
+		page2.kiosk.setText(text);
 		record[2]=burgerprice[tmpt];
 		page2.prices+=Integer.parseInt(record[2]);
+		page2.list+=record[0]+"   "+record[1]+"   "+record[2]+"원\n";
 		model.addRow(record);
 		model2.addRow(record);
 	}
