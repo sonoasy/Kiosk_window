c07e38f5c3d4a2ece2a41bf8f82f0cd81c215f16
seoyeon ahn <61787171+sonoasy@users.noreply.github.com>
2021-12-09 12:59:58 +0900

    홈메뉴

--- page1.java
+++ page1.java
@@ -28,7 +28,7 @@ import javax.swing.JPanel;
 import javax.sound.sampled.AudioSystem;
 
 import javax.sound.sampled.AudioInputStream;
-public class page1 extends JFrame { //swing이 들어옴
+public class page1 extends JFrame { 
     //키오스크 안내 자막
 	public static JLabel kiosk=new JLabel();
 	
@@ -49,13 +49,13 @@ public class page1 extends JFrame { //swing이 들어옴
         alarm.setBounds(300,10,400,300);
         //이미지 크기 맞추기
         //매장식사 이미지
-        ImageIcon setmenu=new ImageIcon("C:\\Users\\AhnSeoYeon\\Desktop\\workspace\\ch09\\src\\ch09\\setmenu.PNG");
+        ImageIcon setmenu=new ImageIcon("C:\\Users\\AhnSeoYeon\\Desktop\\workspace\\ch09\\src\\ch09\\setmenu.PNG"); //매장 식사 이미지, 깃허브에서 menu에서 setmenu를 다운받아 경로를 설정해주어야 합니다
         Image setmenuimg =setmenu.getImage();
         Image newsetmenuimg=setmenuimg.getScaledInstance(300, 200, java.awt.Image.SCALE_SMOOTH);
         ImageIcon newsetmenu=new ImageIcon(newsetmenuimg);
         JLabel setmenus=new JLabel(newsetmenu,JLabel.CENTER);
         //포장 이미지
-        ImageIcon take=new ImageIcon("C:\\Users\\AhnSeoYeon\\Desktop\\workspace\\ch09\\src\\ch09\\takeout.PNG");
+        ImageIcon take=new ImageIcon("C:\\Users\\AhnSeoYeon\\Desktop\\workspace\\ch09\\src\\ch09\\takeout.PNG"); //포장 이미지, 깃허브에서 menu에서 takeout을 다운받아 경로를 설정해주어야 합니다
         Image takeoutf =take.getImage();
         Image takeoutimg=takeoutf.getScaledInstance(180, 200, java.awt.Image.SCALE_SMOOTH);
         ImageIcon newtakeout=new ImageIcon(takeoutimg);
@@ -86,11 +86,11 @@ public class page1 extends JFrame { //swing이 들어옴
 		
 		//키오스크 안내 자막
 		
-		kiosk.setText("지금 듣고 있어요.");
+		kiosk.setText("지금 듣고 있어요."); //자막 초기 메세지 
 		kiosk.setBounds(200,700,600,50);   //x,y, 가로,세로
 		kiosk.setOpaque(true);
 		kiosk.setHorizontalAlignment(JLabel.CENTER);
-        kiosk.setFont(new Font("고딕체", Font.BOLD, 40));
+        kiosk.setFont(new Font("고딕체", Font.BOLD, 30));
         kiosk.setForeground(Color.WHITE);
         kiosk.setBackground(Color.BLACK);
        
@@ -101,36 +101,10 @@ public class page1 extends JFrame { //swing이 들어옴
 		setVisible(true);
 		
         //포장 or 테이크아웃 인텐트와 쳇봇 답변, 답변을 음성으로 바꾼 tts를 입력받으면 실행
-        
         //1. 인텐트 입력과 checklist의 포장여부 값에 따라 버튼 눌리게함
-          //1이 매장, 2가 포장이라 가정
-       // takeoutchoice;
-        if(takeoutchoice==1) {
-        	kiosk.setText("매장에서 식사 선택하였습니다.맞습니까?");
-        	
-        	
-        	
-        	//정보는 챗봇에서 넘어왔므로 화면 전환하는거 실행하기
-        	
-        	
-        }
-        else if(takeoutchoice==2) {
-        
-        	kiosk.setText("테이크 아웃 하시겠습니까?");
-        }
-        
-        
-        
-        
-        
-        
-        
         //2. JLabel kiosk에 답변 글씨가 나타나도록 함 
-        
-        
-        
         //3. 음성을 틀음
-        
+		//위의 3가지 과정을 통해 음성 대화를 진행해야 하나, stt,tts,챗봇이 코랩에서 실행되어 해당 과정은 버튼이 눌리면 자막이 생성되는 방식으로 생략하였습니다. 
         JButton end =new JButton("다음");
         end.setBounds(840,10,100,30);
         end.addActionListener(new ActionListener() {
@@ -149,12 +123,12 @@ public class page1 extends JFrame { //swing이 들어옴
            JButton choice=(JButton)e.getSource();
            
            if(choice.getText().equals("매장에서 식사")) {
-        	   page1.this.kiosk.setText("매장에서 식사 선택하였습니다.맞습니까?");
+        	   page1.this.kiosk.setText("매장에서 식사 선택하였습니다.맞습니까?"); //자막 생성
            }
            else if(choice.getText().equals("테이크 아웃")) {
         	   takeoutchoice=2;
         	   
-        	   page1.this.kiosk.setText("테이크 아웃 하시겠습니까?");
+        	   page1.this.kiosk.setText("테이크 아웃 하시겠습니까?"); //자막 셍성
         	   
            }
        }
@@ -170,22 +144,6 @@ public class page1 extends JFrame { //swing이 들어옴
 		
 		page1 frame=new page1();
 		
-			try { File file = new File("C:\\Users\\AhnSeoYeon\\Desktop\\takeoutt.wav");//voicebooking-speech.wav
-			     TimeUnit.SECONDS.sleep(2);
-			    Clip clip = AudioSystem.getClip(); 
-			   clip.open(AudioSystem.getAudioInputStream(file));
-			//clip.start(); 
-			TimeUnit.SECONDS.sleep(3);
-			//kiosk.setText("테이크 아웃 하시겠습니까?");
-			
-			}
-			catch (Exception e) { System.err.println("Put the music.wav file in the sound folder if you want to play background music, only optional!");
-			} 
-			
-		
- 	   
-		
-
 	}
 
 }
